17844474f2debc72dc831095b99351d3b0ebc18a
Jeff Upton <jeff.upton@sailpoint.com>
2016-09-05 14:14:18 -0500

    initial commit

---
+++ include/ocr/log.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright 2016 Jeff Upton
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+
+typedef enum ocr_log_level {
+  OCR_LOG_TRACE,
+  OCR_LOG_DEBUG,
+  OCR_LOG_INFO,
+  OCR_LOG_WARN,
+  OCR_LOG_ERROR
+} ocr_log_level_t;
