9e891637122d7fb8f3481d9007a46053e6523fd5
Jeff Upton <jeff.upton@sailpoint.com>
2016-09-06 13:54:53 -0500

    integrate google test

--- test/main.cpp
+++ test/main.cpp
@@ -1,6 +1,8 @@
+#include <gtest/gtest.h>
+
 #include <ocr/ocr.h>
 
-int main(int argc, char **argv)
+TEST(General, PoolAllocation)
 {
     ocr_pool_t *pool;
     if (OCR_SUCCEEDED(ocr_pool_create(ocr_mb(32), NULL, &pool))) {
@@ -8,19 +10,28 @@ int main(int argc, char **argv)
         if (OCR_SUCCEEDED(ocr_read_file(__FILE__, pool, &buffer))) {
             OCR_INFO("Read Buffer: %s", buffer.data);
         }
+
         ocr_pool_clear(pool);
+
         if (OCR_SUCCEEDED(ocr_read_file(__FILE__, pool, &buffer))) {
             OCR_INFO("Read Buffer: %s", buffer.data);
         }
 
         ocr_pool_destroy(pool);
     }
+}
 
+TEST(General, LogLevels)
+{
     OCR_TRACE("Test Message");
     OCR_DEBUG("Test Message");
     OCR_INFO("Test Message");
     OCR_WARN("Test Message");
     OCR_ERROR("Test Message");
+}
 
-    return 0;
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
